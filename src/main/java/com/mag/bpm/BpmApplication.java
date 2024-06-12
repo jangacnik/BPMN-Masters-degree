95eb74d41ecd52c88428a3e8f75cee456ec285fb
Jan Gacnik <nqmrnr7xgg@privaterelay.appleid.com>
2024-06-12 17:45:21 +0200

    Initial Camunda initialization & config

--- src/main/java/com/mag/bpm/BpmApplication.java
+++ src/main/java/com/mag/bpm/BpmApplication.java
@@ -1,13 +1,15 @@
 package com.mag.bpm;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class BpmApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(BpmApplication.class, args);
   }
+
 
 }
